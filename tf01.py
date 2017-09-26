1ec115fee4cbe4d1458bbb16323607e87c55228d
t162880 <t162880@MMSURMAWUW8.mmi.mig.corp>
2017-09-26 09:28:25 -0400

    train simple linear

--- tf01.py
+++ tf01.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+
 node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0) # also tf.float32 implicitly
 #print(node1, node2)
@@ -29,10 +30,17 @@ sess.run(init)
 y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+#print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
 fixW = tf.assign(W, [-1.])
 fixb = tf.assign(b, [1.])
 sess.run([fixW, fixb])
-print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+#print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+sess.run(init) # reset values to incorrect defaults.
+for i in range(1000):
+  sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+
+print(sess.run([W, b]))
